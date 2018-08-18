ece524515170b24c93b51f255abdeb41f106f7c2
Daniel Cassidy <mail@danielcassidy.me.uk>
2018-08-18 22:22:43 +0100

    Apply tslint rules to self.

--- index.ts
+++ index.ts
@@ -1,10 +1,84 @@
 export = {
-    "extends": ["tslint:recommended", "tslint-eslint-rules"],
-    "rules": {
+    extends: ["tslint:recommended", "tslint-eslint-rules"],
+    jsRules: {
+        "comment-format": {
+            options: "check-space"
+        },
+        "encoding": true,
+        "indent": false,
+        "no-bitwise": false,
+        "no-consecutive-blank-lines": {
+            options: [2]
+        },
+        "no-duplicate-switch-case": true,
+        "no-duplicate-variable": true,
+        "no-empty": false,
+        "no-empty-character-class": true,
+        "no-implicit-dependencies": true,
+        "no-invalid-regexp": true,
+        "no-irregular-whitespace": true,
+        "no-multi-spaces": true,
+        "no-shadowed-variable": false,
+        "no-switch-case-fall-through": true,
+        "no-unexpected-multiline": true,
+        "no-unnecessary-callback-wrapper": true,
+        "no-unsafe-finally": true,
+        "no-unused-variable": true,
+        "object-literal-key-quotes": {
+            options: ["consistent-as-needed"]
+        },
+        "prefer-function-over-method": true,
+        "prefer-while": true,
+        "ter-indent": {
+            options: [4, {
+                CallExpression: {
+                    arguments: 1
+                },
+                FunctionDeclaration: {
+                    body: 1,
+                    parameters: "first"
+                },
+                FunctionExpression: {
+                    body: 1,
+                    parameters: "first"
+                },
+                MemberExpression: 1,
+                SwitchCase: 0,
+                VariableDeclarator: 1,
+                outerIIFEBody: 1
+            }]
+        },
+        "ter-no-proto": true,
+        "ter-no-script-url": true,
+        "ter-no-self-compare": true,
+        "ter-no-tabs": true,
+        "trailing-comma": {
+            options: {
+                esSpecCompliant: true,
+                multiline: "ignore",
+                singleline: "never"
+            }
+        },
+        "variable-name": {
+            options: ["check-format"]
+        },
+        "whitespace": {
+            options: [
+                "check-branch",
+                "check-decl",
+                "check-operator",
+                "check-rest-spread",
+                "check-separator",
+                "check-type",
+                "check-type-operator"
+            ]
+        }
+    },
+    rules: {
         "arrow-parens": false,
         "await-promise": true,
         "comment-format": {
-            "options": "check-space"
+            options: "check-space"
         },
         "encoding": true,
         "indent": false,
@@ -14,7 +88,7 @@ export = {
         "no-bitwise": false,
         "no-boolean-literal-compare": true,
         "no-consecutive-blank-lines": {
-            "options": [2]
+            options: [2]
         },
         "no-duplicate-imports": true,
         "no-duplicate-switch-case": true,
@@ -37,41 +111,41 @@ export = {
         "no-unexpected-multiline": true,
         "no-unnecessary-callback-wrapper": true,
         "no-unnecessary-class": {
-            "options": ["allow-constructor-only"]
+            options: ["allow-constructor-only"]
         },
         "no-unnecessary-type-assertion": true,
         "no-unsafe-finally": true,
         "no-unused-variable": true,
         "no-var-keyword": true,
         "no-void-expression": {
-            "options": ["ignore-arrow-function-shorthand"]
+            options: ["ignore-arrow-function-shorthand"]
         },
         "object-literal-key-quotes": {
-            "options": ["consistent-as-needed"]
+            options: ["consistent-as-needed"]
         },
         "prefer-function-over-method": true,
         "prefer-object-spread": true,
         "prefer-while": true,
         "space-within-parens": {
-            "options": [0]
+            options: [0]
         },
         "ter-indent": {
-            "options": [4, {
-                "SwitchCase": 0,
-                "VariableDeclarator": 1,
-                "outerIIFEBody": 1,
-                "MemberExpression": 1,
-                "FunctionDeclaration": {
-                    "parameters": "first",
-                    "body": 1
+            options: [4, {
+                CallExpression: {
+                    arguments: 1
                 },
-                "FunctionExpression": {
-                    "parameters": "first",
-                    "body": 1
+                FunctionDeclaration: {
+                    body: 1,
+                    parameters: "first"
                 },
-                "CallExpression": {
-                    "arguments": 1
-                }
+                FunctionExpression: {
+                    body: 1,
+                    parameters: "first"
+                },
+                MemberExpression: 1,
+                SwitchCase: 0,
+                VariableDeclarator: 1,
+                outerIIFEBody: 1
             }]
         },
         "ter-no-proto": true,
@@ -79,97 +153,23 @@ export = {
         "ter-no-self-compare": true,
         "ter-no-tabs": true,
         "trailing-comma": {
-            "options": {
-                "multiline": "ignore",
-                "singleline": "never",
-                "esSpecCompliant": true
+            options: {
+                esSpecCompliant: true,
+                multiline: "ignore",
+                singleline: "never"
             }
         },
         "typedef": {
-            "options": [
+            options: [
                 "call-signature",
                 "parameter"
             ]
         },
         "variable-name": {
-            "options": ["check-format"]
+            options: ["check-format"]
         },
         "whitespace": {
-            "options": [
-                "check-branch",
-                "check-decl",
-                "check-operator",
-                "check-rest-spread",
-                "check-separator",
-                "check-type",
-                "check-type-operator"
-            ]
-        }
-    },
-    "jsRules": {
-        "comment-format": {
-            "options": "check-space"
-        },
-        "encoding": true,
-        "indent": false,
-        "no-bitwise": false,
-        "no-consecutive-blank-lines": {
-            "options": [2]
-        },
-        "no-duplicate-switch-case": true,
-        "no-duplicate-variable": true,
-        "no-empty": false,
-        "no-empty-character-class": true,
-        "no-implicit-dependencies": true,
-        "no-invalid-regexp": true,
-        "no-irregular-whitespace": true,
-        "no-multi-spaces": true,
-        "no-shadowed-variable": false,
-        "no-switch-case-fall-through": true,
-        "no-unexpected-multiline": true,
-        "no-unsafe-finally": true,
-        "no-unnecessary-callback-wrapper": true,
-        "no-unused-variable": true,
-        "object-literal-key-quotes": {
-            "options": ["consistent-as-needed"]
-        },
-        "prefer-function-over-method": true,
-        "prefer-while": true,
-        "ter-indent": {
-            "options": [4, {
-                "SwitchCase": 0,
-                "VariableDeclarator": 1,
-                "outerIIFEBody": 1,
-                "MemberExpression": 1,
-                "FunctionDeclaration": {
-                    "parameters": "first",
-                    "body": 1
-                },
-                "FunctionExpression": {
-                    "parameters": "first",
-                    "body": 1
-                },
-                "CallExpression": {
-                    "arguments": 1
-                }
-            }]
-        },
-        "ter-no-proto": true,
-        "ter-no-script-url": true,
-        "ter-no-self-compare": true,
-        "ter-no-tabs": true,
-        "trailing-comma": {
-            "options": {
-                "multiline": "ignore",
-                "singleline": "never",
-                "esSpecCompliant": true
-            }
-        },
-        "variable-name": {
-            "options": ["check-format"]
-        },
-        "whitespace": {
-            "options": [
+            options: [
                 "check-branch",
                 "check-decl",
                 "check-operator",
@@ -180,4 +180,4 @@ export = {
             ]
         }
     }
-}
+};
